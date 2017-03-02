89dbf5fd9740600878489e72ff527ca31fb88ebf
DenkerMayr <andreas.denkmayr@gmail.com>
2017-03-02 22:29:05 +0100

    Initial commit of DerGeraet.rb

---
+++ DerGeraet.rb
@@ -0,0 +1,25 @@
+::RBNACL_LIBSODIUM_GEM_LIB_PATH = "D:/Ruby/Dependencies/Discord/libsodium.dll"
+require 'discordrb'
+
+bot = Discordrb::Commands::CommandBot.new token: 'Mjg2OTU2NzU0MjUwNDk4MDQ5.C5oRXA.yLI0pJcpWJfF8qJYKNKUIAlwGVM', client_id: 286956754250498049, prefix: '!'
+
+# This method call adds an event handler that will be called on any message that exactly contains the string "Ping!".
+# The code inside it will be executed, and a "Pong!" response will be sent to the channel.
+bot.message(with_text: 'Ping!') do |event| 
+	event.respond 'Pong!'
+end
+
+bot.message(with_text: 'Dominik') do |event|
+	event.respond 'Hallo Dominik!'
+end
+
+bot.command(:exit, help_available: false) do |event|
+  # This is a check that only allows a user with a specific ID to execute this command. Otherwise, everyone would be
+  # able to shut your bot down whenever they wanted.
+  break unless event.user.id == 173752508265660416 # Replace number with your ID
+
+  bot.send_message(event.channel.id, 'Bot is shutting down')
+  exit
+end
+
+bot.run
